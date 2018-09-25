053ffdb75316196b0dc84ad73b30e41bd897c03c
CardB <adam.m.denny2@gmail.com>
2018-09-25 11:11:12 -0700

    es5 convert

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 'use strict';
-module.exports = (self, options) => {
+
+module.exports = function (self, options) {
 	options = Object.assign({}, options);
 
-	const filter = key => {
-		const match = pattern => typeof pattern === 'string' ? key === pattern : pattern.test(key);
+	var filter = function filter(key) {
+		var match = function match(pattern) {
+			return typeof pattern === 'string' ? key === pattern : pattern.test(key);
+		};
 
 		if (options.include) {
 			return options.include.some(match);
@@ -16,36 +19,41 @@ module.exports = (self, options) => {
 		return true;
 	};
 
-	for (const key of Object.getOwnPropertyNames(self.constructor.prototype)) {
-		const value = self[key];
+	var _iteratorNormalCompletion = true;
+	var _didIteratorError = false;
+	var _iteratorError = undefined;
 
-		if (key !== 'constructor' && typeof value === 'function' && filter(key)) {
-			self[key] = value.bind(self);
+	try {
+		for (var _iterator = Object.getOwnPropertyNames(self.constructor.prototype)[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+			var key = _step.value;
+
+			var val = self[key];
+
+			if (key !== 'constructor' && typeof val === 'function' && filter(key)) {
+				self[key] = val.bind(self);
+			}
+		}
+	} catch (err) {
+		_didIteratorError = true;
+		_iteratorError = err;
+	} finally {
+		try {
+			if (!_iteratorNormalCompletion && _iterator.return) {
+				_iterator.return();
+			}
+		} finally {
+			if (_didIteratorError) {
+				throw _iteratorError;
+			}
 		}
 	}
 
 	return self;
 };
 
-const excludedReactMethods = [
-	'componentWillMount',
-	'UNSAFE_componentWillMount',
-	'render',
-	'getSnapshotBeforeUpdate',
-	'componentDidMount',
-	'componentWillReceiveProps',
-	'UNSAFE_componentWillReceiveProps',
-	'shouldComponentUpdate',
-	'componentWillUpdate',
-	'UNSAFE_componentWillUpdate',
-	'componentDidUpdate',
-	'componentWillUnmount',
-	'componentDidCatch',
-	'setState',
-	'forceUpdate'
-];
+var excludedReactMethods = ['componentWillMount', 'UNSAFE_componentWillMount', 'render', 'getSnapshotBeforeUpdate', 'componentDidMount', 'componentWillReceiveProps', 'UNSAFE_componentWillReceiveProps', 'shouldComponentUpdate', 'componentWillUpdate', 'UNSAFE_componentWillUpdate', 'componentDidUpdate', 'componentWillUnmount', 'componentDidCatch', 'setState', 'forceUpdate'];
 
-module.exports.react = (self, options) => {
+module.exports.react = function (self, options) {
 	options = Object.assign({}, options);
 	options.exclude = (options.exclude || []).concat(excludedReactMethods);
 	return module.exports(self, options);
